1cb42d39655633d844a2e864915316aa1ab4e741
Elisaavann <113943522+Elisaavann@users.noreply.github.com>
2023-01-18 17:55:38 +0500

    Add files via upload
    api vacancies table

--- fullstack/load_last_vacancies.py
+++ fullstack/load_last_vacancies.py
@@ -1,21 +1,59 @@
 import json
 import requests
+import re
+from datetime import datetime, timedelta 
 
-def getLastVacancies(name):
-    req = requests.get('https://api.hh.ru/vacancies', {'text': f'Name: {name}'})
-    hhdata = req.content.decode()
-    req.close()
-    res = []
-    for v in json.loads(hhdata)['items'][:10]:
+def getLastVacancies(listKeysForNameVac, vacCountMax = 10):
+    lastVacHeader = [('name','Название вакансии'), ('description', 'Описание вакансии'),
+        ('key_skills', 'Навыки'), ('employer', 'Компания'), ('salary', 'Средний Оклад'),
+        ('area','Название региона'), ('published_at', 'Дата публикации вакансии')]
+    hhlistfull = []
+    hhlist_id = []
+    for keyForNameVac in listKeysForNameVac:
+        req = requests.get('https://api.hh.ru/vacancies?text=NAME:' + keyForNameVac)
+        hhdata = req.content.decode()
+        req.close()
+        hhlist = json.loads(hhdata)['items']
+        for v in hhlist:
+            if ((v['id'] not in hhlist_id) and 
+                    ((v['type']['id'] == 'open') or (v['type']['name'] == 'Открытая'))):
+                hhlist_id.append(v['id'])
+                hhlistfull.append(v)
+    hhlistfull.sort(key=lambda v: format_data(v['published_at'])['abs'], reverse=True)
+    hhLastVacTop = []
+    vacCount = 0
+    for v in hhlistfull:
         req = requests.get(v['url'])
         hhdata = req.content.decode()
         req.close()
-        res.append(json.loads(hhdata))
-    return(res)
+        hhlist = json.loads(hhdata)
+        hhlist['description'] = ' '.join(re.sub(r'\<[^>]*\>', '', hhlist['description']).split()) 
+        hhlist['key_skills'] = ', '.join([ks['name'] for ks in hhlist['key_skills']])
+        hhlist['employer'] = hhlist['employer']['name']
+        if hhlist['salary'] == None: continue
+        hhlist['salary']['to'] = hhlist['salary']['from'] if hhlist['salary']['to'] == None else hhlist['salary']['to']
+        hhlist['salary']['from'] = hhlist['salary']['to'] if hhlist['salary']['from'] == None else hhlist['salary']['from']
+        if hhlist['salary']['from'] == None: continue
+        hhlist['salary']['currency'] = 'RUR' if hhlist['salary']['currency'] == None else hhlist['salary']['currency']
+        hhlist['salary'] = f"{int((float(hhlist['salary']['from']) + float(hhlist['salary']['to']))/2)} {hhlist['salary']['currency']}" 
+        hhlist['area'] = hhlist['area']['name']
+        hhlist = [hhlist[h[0]] for h in lastVacHeader]
+        hhLastVacTop.append(hhlist)
+        vacCount += 1
+        if vacCount == vacCountMax: break
+    return ([h[1] for h in lastVacHeader], hhLastVacTop)
+
+def format_data(publ):
+    t = datetime.strptime(publ[:-5], '%Y-%m-%dT%H:%M:%S')
+    dt = timedelta(hours=int(publ[-4:-2]), minutes=int(publ[-2:]))
+    utc = t - dt if publ[-5] == '+' else t + dt
+    return {'date': ' '.join(publ.split('T')), 'abs': int(datetime.timestamp(utc))}
     
-lastVachh = []
-res = getLastVacancies('fullstack')
-print(res)
-lastVachh.append(res)
+res = getLastVacancies(['fullstack', 'full-stack', 'full stack', 'фулстак', 'фуллстак', 'фулстэк', 'фуллстэк'], 10)
+print(res[0])
+print(res[1])
+
 
 xxx = 1
+
+
--- profinfo/views.py
+++ profinfo/views.py
@@ -5,6 +5,8 @@ from profinfo.models import DynamicsSalaryByYear, DynamicsVacanciesByYear, Dynam
     DynamicsVacanciesByYearForProf, SalaryLevelsByCity, FractionVacanciesByCity, TopSkills
 import json
 import requests
+import re
+from datetime import datetime, timedelta 
 
 # Create your views here.
 
@@ -53,46 +55,79 @@ def skills(request):
         i.count = "{:.2%}".format(i.count/100)
     skills_all = TopSkills.objects.exclude(year = 0)
     years = sorted(set(str(i.year) for i in skills_all))
-    numb = [i for i in range(10)]
     skills_year0 = {}
-    for yr in years:
-        skills_year0[yr] = {}
-        n = 0
-        for j in TopSkills.objects.filter(year = yr).order_by('-count'):
-            if n == 10: break
-            skills_year0[yr][str(n)] = (j.skills, j.count)
-            n += 1
+    for c in range(len(years)):
+        sk_year = TopSkills.objects.filter(year = years[c])
+        skills_year0[c] = []
+        for sk in sk_year:
+            skills_year0[c].append((sk.skills.title(), sk.count))
+        skills_year0[c].sort(key = lambda el: el[1], reverse = True)
     skills_year = {}
-    for n in numb:
-        skills_year[str(n)] = (skills_year0[years[0]][str(n)][0],skills_year0[years[0]][str(n)][1],skills_year0[years[1]][str(n)][0],skills_year0[years[1]][str(n)][1],
-                          skills_year0[years[2]][str(n)][0],skills_year0[years[2]][str(n)][1],skills_year0[years[3]][str(n)][0],skills_year0[years[3]][str(n)][1],
-                          skills_year0[years[4]][str(n)][0],skills_year0[years[4]][str(n)][1],skills_year0[years[5]][str(n)][0],skills_year0[years[5]][str(n)][1],
-                          skills_year0[years[6]][str(n)][0],skills_year0[years[6]][str(n)][1],skills_year0[years[7]][str(n)][0],skills_year0[years[7]][str(n)][1])
+    for r in range(10):
+        skills_year[r] = []
+        for c in range(len(years)):
+            skills_year[r].append(skills_year0[c][r] if skills_year0[c][r] else ('',''))
     template = loader.get_template('skills.html')
     context = {
-        'numb': numb,
         'years': years,
         'skills_web': skills_web,
-        'skills_year': skills_year,
+        'skills_year': skills_year
     }
     return HttpResponse(template.render(context, request))
 
 def lastVac(request):
-    req = requests.get('https://api.hh.ru/vacancies', {'text': 'Name: fullstack'})
-    hhdata = req.content.decode()
-    req.close()
-    res = []
-    for v in json.loads(hhdata)['items'][:10]:
-        req = requests.get(v['url'])
-        hhdata = req.content.decode()
-        req.close()
-        res.append(json.loads(hhdata))
-    
-    print(res)
-    
+    listKeysForNameVac = ['fullstack', 'full-stack', 'full stack', 'фулстак', 'фуллстак', 'фулстэк', 'фуллстэк']
+    res = getLastVacancies(listKeysForNameVac, 10)
     template = loader.get_template('lastVac.html')
     context = {
-        'last_vacancy': res
+        'lastVacHeader': res[0],
+        'lastTopVacancy': res[1]
     }
     return HttpResponse(template.render(context, request))
 
+def getLastVacancies(listKeysForNameVac, vacCountMax = 10):
+    lastVacHeader = [('name','Название вакансии'), ('description', 'Описание вакансии'), ('key_skills', 'Навыки'),
+        ('employer', 'Компания'), ('salary', 'Средний Оклад'), ('area','Название региона'), ('published_at', 'Дата публикации вакансии')]
+    hhlistfull = []
+    hhlist_id = []
+    for keyForNameVac in listKeysForNameVac:
+        req = requests.get('https://api.hh.ru/vacancies?text=NAME:' + keyForNameVac)
+        hhdata = req.content.decode()
+        req.close()
+        hhlist = json.loads(hhdata)['items']
+        for v in hhlist:
+            if ((v['id'] not in hhlist_id) and 
+                    ((v['type']['id'] == 'open') or (v['type']['name'] == 'Открытая'))):
+                hhlist_id.append(v['id'])
+                hhlistfull.append(v)
+    hhlistfull.sort(key=lambda v: format_data(v['published_at'])['abs'], reverse=True)
+    hhLastVacTop = []
+    vacCount = 0
+    for v in hhlistfull:
+        req = requests.get(v['url'])
+        hhdata = req.content.decode()
+        req.close()
+        hhlist = json.loads(hhdata)
+        hhlist['description'] = ' '.join(re.sub(r'\<[^>]*\>', '', hhlist['description']).split()) 
+        hhlist['key_skills'] = ', '.join([ks['name'] for ks in hhlist['key_skills']])
+        hhlist['employer'] = hhlist['employer']['name']
+        if hhlist['salary'] == None: continue
+        hhlist['salary']['to'] = hhlist['salary']['from'] if hhlist['salary']['to'] == None else hhlist['salary']['to']
+        hhlist['salary']['from'] = hhlist['salary']['to'] if hhlist['salary']['from'] == None else hhlist['salary']['from']
+        if hhlist['salary']['from'] == None: continue
+        hhlist['salary']['currency'] = 'RUR' if hhlist['salary']['currency'] == None else hhlist['salary']['currency']
+        hhlist['salary'] = f"{int((float(hhlist['salary']['from']) + float(hhlist['salary']['to']))/2)} {hhlist['salary']['currency']}" 
+        hhlist['area'] = hhlist['area']['name']
+        hhlist['published_at'] = format_data(hhlist['published_at'])['date']
+        hhlist = [hhlist[h[0]] for h in lastVacHeader]
+        hhLastVacTop.append(hhlist)
+        vacCount += 1
+        if vacCount == vacCountMax: break
+    return ([h[1] for h in lastVacHeader], hhLastVacTop)
+
+def format_data(publ):
+    t = datetime.strptime(publ[:-5], '%Y-%m-%dT%H:%M:%S')
+    dt = timedelta(hours=int(publ[-4:-2]), minutes=int(publ[-2:]))
+    utc = t - dt if publ[-5] == '+' else t + dt
+    return {'date': f"{datetime.strftime(t, '%d.%m.%Y')} {publ[-13:-5]} {publ[-5:-2]}:{publ[-2:]}:00", 'abs': int(datetime.timestamp(utc))}
+    
